d9654ce56f9bd15620a9e62535641c1ed9f92d3c
Andres Osorio <andres.osoriogr@gmail.com>
2026-04-22 19:57:48 -0500

    track from fedora

---
+++ scripts/fedora_update.sh
@@ -0,0 +1,9 @@
+sudo dnf upgrade --refresh -y
+sudo dnf autoremove -y && sudo dnf clean all -y
+
+fwupdmgr refresh --force
+fwupdmgr get-updates
+sudo fwupdmgr update
+
+gsettings set org.gnome.desktop.input-sources xkb-options "['ctrl:nocaps']"
+gsettings get org.gnome.desktop.input-sources xkb-options
