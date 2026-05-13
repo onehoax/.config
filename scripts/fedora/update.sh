339e5d0a4181802e322962abde700ccb2a4136e7
Andres Osorio <andres.osoriogr@gmail.com>
2026-05-13 12:54:27 -0500

    fedora upgrade

--- scripts/fedora/update.sh
+++ scripts/fedora/update.sh
@@ -4,6 +4,9 @@ dnf check-update
 sudo dnf upgrade --refresh -y
 sudo dnf autoremove -y && sudo dnf clean all -y
 
+flatpak update
+flatpak uninstall --unused
+
 fwupdmgr refresh --force
 fwupdmgr get-updates
 sudo fwupdmgr update
