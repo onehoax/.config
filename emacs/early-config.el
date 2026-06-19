9b7c4e3c0c35dcf897236bac991452d00c606cf7
Andres Osorio <andres.osoriogr@gmail.com>
2026-06-19 08:52:59 -0500

    emacs: early init

--- emacs/early-config.el
+++ emacs/early-config.el
@@ -2,11 +2,11 @@
 
 ;; Frame settings on init and subsequent frames created
 (setq initial-frame-alist
-      '((width . 150)
-        (height . 50)
+      '((fullscreen . maximized)
+        ;; (width . 150)
+        ;; (height . 50)
         (vertical-scroll-bars . nil)
         (tool-bar-lines . 0)
         (menu-bar-lines . 0))
-      ;;'(fullscreen . maximized)
 
       default-frame-alist initial-frame-alist)
