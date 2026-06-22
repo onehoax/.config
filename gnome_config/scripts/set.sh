f5d7ff53d7f505a964b25772f6197721786d27bd
Andres Osorio <andres.osoriogr@gmail.com>
2026-06-22 08:38:43 -0500

    vscode, gnome

--- gnome_config/scripts/set.sh
+++ gnome_config/scripts/set.sh
@@ -4,11 +4,12 @@
 # Backup/restore groups of settings: dconf dump and dconf load
 # You can use dconf to set settings as well but gsettings is a better interface for setting
 
+gsettings set org.gnome.settings-daemon.plugins.media-keys screensaver "[]"
 gsettings set org.gnome.desktop.input-sources xkb-options "['ctrl:swap_lalt_lctl', 'caps:escape']"
-gsettings set org.gnome.mutter.keybindings toggle-tiled-left "['<Super>Left']"
-gsettings set org.gnome.mutter.keybindings toggle-tiled-right "['<Super>Right']"
 gsettings set org.gnome.desktop.wm.keybindings close "['<Super>q']"
+gsettings set org.gnome.mutter.keybindings toggle-tiled-left "['<Super>h']"
+gsettings set org.gnome.mutter.keybindings toggle-tiled-right "['<Super>l']"
 gsettings set org.gnome.desktop.wm.keybindings switch-group "['<Super>grave']"
 gsettings set org.gnome.desktop.wm.keybindings toggle-maximized "['<Super>m']"
-gsettings set org.gnome.desktop.wm.keybindings move-to-monitor-left "['<Super><Shift>Left']"
-gsettings set org.gnome.desktop.wm.keybindings move-to-monitor-right "['<Super><Shift>Right']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-monitor-left "['<Super><Shift>h']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-monitor-right "['<Super><Shift>l']"
